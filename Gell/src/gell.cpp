ae31b3c8c60d79c5a6d2c45f2999890cb890678e
Илья <heksen84@yandex.ru>
2017-09-01 12:05:02 +0600

    ok

--- Gell/src/gell.cpp
+++ Gell/src/gell.cpp
@@ -120,10 +120,10 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 				
 		glBegin(GL_QUADS);
-			glColor3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex2f(-0.5f, -0.5f); 
 			glVertex2f( 0.5f, -0.5f);
-			glColor3f(1.0f, 0.0f, 0.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
 			glVertex2f( 0.5f,  0.5f);
 			glVertex2f(-0.5f,  0.5f);
 		glEnd();
