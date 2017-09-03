a8e228fd0fdbaba54ca2db8c83786ae08f51eb81
Илья <heksen84@yandex.ru>
2017-09-03 14:13:16 +0600

    up

---
+++ Gell/src/Player.cpp
@@ -0,0 +1,18 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: 03 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#include "Player.h"
+
+Player::Player() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Player::~Player() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/Player.h
@@ -0,0 +1,17 @@
+/*
+ * Player.h
+ *
+ *  Created on: 03 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+class Player {
+public:
+	Player();
+	virtual ~Player();
+};
+
+#endif /* PLAYER_H_ */
