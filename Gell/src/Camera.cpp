5dbee886931545e7209b2ac9c14f2feb517bc592
Илья <heksen84@yandex.ru>
2017-09-04 22:49:03 +0600

    up

---
+++ Gell/src/Camera.cpp
@@ -0,0 +1,18 @@
+/*
+ * Camera.cpp
+ *
+ *  Created on: 04 сент. 2017 г.
+ *      Author: Илья
+ */
+
+#include "Camera.h"
+
+Camera::Camera() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Camera::~Camera() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/Camera.h
@@ -0,0 +1,17 @@
+/*
+ * Camera.h
+ *
+ *  Created on: 04 сент. 2017 г.
+ *      Author: Илья
+ */
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+class Camera {
+public:
+	Camera();
+	virtual ~Camera();
+};
+
+#endif /* CAMERA_H_ */
