4d409179ba058919e610f12bc542538293048994
Илья <heksen84@yandex.ru>
2017-09-05 20:11:22 +0600

    +

---
+++ Gell/src/GLFW.cpp
@@ -0,0 +1,18 @@
+/*
+ * GLFW.cpp
+ *
+ *  Created on: 05 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#include "GLFW.h"
+
+GLFW::GLFW() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GLFW::~GLFW() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/GLFW.h
@@ -0,0 +1,17 @@
+/*
+ * GLFW.h
+ *
+ *  Created on: 05 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef GLFW_H_
+#define GLFW_H_
+
+class GLFW {
+public:
+	GLFW();
+	virtual ~GLFW();
+};
+
+#endif /* GLFW_H_ */
