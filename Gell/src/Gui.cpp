ef6712afb180a7eb8975bfcb1789a989cf225b53
Илья <heksen84@yandex.ru>
2017-09-06 20:03:20 +0600

    +

---
+++ Gell/src/Gui.cpp
@@ -0,0 +1,18 @@
+/*
+ * Gui.cpp
+ *
+ *  Created on: 06 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#include "Gui.h"
+
+Gui::Gui() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Gui::~Gui() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/Gui.h
@@ -0,0 +1,17 @@
+/*
+ * Gui.h
+ *
+ *  Created on: 06 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef GUI_H_
+#define GUI_H_
+
+class Gui {
+public:
+	Gui();
+	virtual ~Gui();
+};
+
+#endif /* GUI_H_ */
