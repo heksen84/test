4c0a04578f042c509d4e05d3ada56093a42c2dfa
Илья <heksen84@yandex.ru>
2017-09-10 12:38:02 +0600

    +

---
+++ Gell/src/SkyBox.cpp
@@ -0,0 +1,18 @@
+/*
+ * SkyBox.cpp
+ *
+ *  Created on: 10 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#include "SkyBox.h"
+
+SkyBox::SkyBox() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SkyBox::~SkyBox() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/SkyBox.h
@@ -0,0 +1,17 @@
+/*
+ * SkyBox.h
+ *
+ *  Created on: 10 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef SKYBOX_H_
+#define SKYBOX_H_
+
+class SkyBox {
+public:
+	SkyBox();
+	virtual ~SkyBox();
+};
+
+#endif /* SKYBOX_H_ */
