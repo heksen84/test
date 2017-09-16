fae6615fe48ad513f3a62e4abefee509192f0f87
Илья <heksen84@yandex.ru>
2017-09-16 20:17:23 +0600

    +

--- Gell/src/Gell.cpp
+++ Gell/src/Gell.cpp
@@ -18,28 +18,40 @@ GLuint vbo=0, vao=0;
 GLuint vs, fs;
 GLuint shader_programm;
 
-const char* vertex_shader =
- "#version 400\n"
- "in vec3 vp;"
- "void main() {"
- "  gl_Position = vec4(vp, 1.0);"
-"}";
-
- const char* fragment_shader =
- "#version 400\n"
- "out vec4 frag_colour;"
- "void main() {"
- "  frag_colour = vec4(1.0, 1.5, 0.5, 1.0);"
- "}";
+// Shader sources
+const GLchar* vertexSource = R"glsl(
+    #version 330 core
+    in vec2 position;
+    in vec3 color;
+    out vec3 Color;
+    void main()
+    {
+        Color = color;
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+const GLchar* fragmentSource = R"glsl(
+    #version 330 core
+    in vec3 Color;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(Color, 1.0);
+    }
+)glsl";
 
 // x, y, z
-float points[] = {
-   -0.5f,  0.5f,  0.0f,
-   -0.5f, -0.5f,  0.0f,
-    0.5f, -0.5f,  0.0f,
-    0.5f,  0.5f,  0.0f
-};
+ float points[] = {
+     -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+      0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+     -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+ };
 
+ GLuint elements[] = {
+     0, 1, 2,
+     2, 3, 0
+ };
 void InitLibs(void)
 {
 	InitGLFW();
@@ -49,34 +61,75 @@ void InitLibs(void)
 
 void CreatePanel()
 {
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+	// Create Vertex Array Object
+	    GLuint vao;
+	    glGenVertexArrays(1, &vao);
+	    glBindVertexArray(vao);
 
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+	    // Create a Vertex Buffer Object and copy the vertex data to it
+	    GLuint vbo;
+	    glGenBuffers(1, &vbo);
 
-	vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
-	glCompileShader(vs);
-	fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
-	glCompileShader(fs);
+	    GLfloat vertices[] = {
+	    	 // x     y    	 r     g     b
+	        -0.25f,  0.4f, 	1.0f, 0.0f, 0.0f, // Top-left
+	         0.25f,  0.4f, 	0.0f, 1.0f, 0.0f, // Top-right
+	         0.25f, -0.4f, 	0.0f, 0.0f, 1.0f, // Bottom-right
+	        -0.25f, -0.4f, 	1.0f, 1.0f, 0.0f  // Bottom-left
+	    };
 
-	shader_programm = glCreateProgram();
-	glAttachShader(shader_programm, fs);
-	glAttachShader(shader_programm, vs);
-	glLinkProgram(shader_programm);
+	    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	    // Create an element array
+	    GLuint ebo;
+	    glGenBuffers(1, &ebo);
+
+	    // первая верхняя вершина (Top-left) и последняя нижняя (Bottom-left) имеют общую вершину
+	    GLuint elements[] = {
+	        0, 1, 2,
+	        2, 3, 0
+	    };
+
+	    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+	    // Create and compile the vertex shader
+	    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	    glCompileShader(vertexShader);
+
+	    // Create and compile the fragment shader
+	    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	    glCompileShader(fragmentShader);
+
+	    // Link the vertex and fragment shader into a shader program
+	    GLuint shaderProgram = glCreateProgram();
+	    glAttachShader(shaderProgram, vertexShader);
+	    glAttachShader(shaderProgram, fragmentShader);
+	    glBindFragDataLocation(shaderProgram, 0, "outColor");
+	    glLinkProgram(shaderProgram);
+	    glUseProgram(shaderProgram);
+
+	    // установить позицию из шейдера
+	    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	    glEnableVertexAttribArray(posAttrib);
+	    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+
+	    // установить цвета из шейдера
+	    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	    glEnableVertexAttribArray(colAttrib);
+	    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
 }
 
 void DrawPanel()
 {
-	glUseProgram(shader_programm);
-	glBindVertexArray(vao);
-	glDrawArrays(GL_QUADS, 0, 4);
+	 // Clear the screen to black
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Draw a rectangle from the 2 triangles using 6 indices
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 /* 
