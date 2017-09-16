4c818024864a66ea5207d6589c6ad35af222dea3
Илья <heksen84@yandex.ru>
2017-09-16 20:55:32 +0600

    +

---
+++ Gell/src/Game/Gui/Window.cpp
@@ -0,0 +1,18 @@
+/*
+ * Window.cpp
+ *
+ *  Created on: 16 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#include "Window.h"
+
+Window::Window() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Window::~Window() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/Game/Gui/Window.h
@@ -0,0 +1,17 @@
+/*
+ * Window.h
+ *
+ *  Created on: 16 сент. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef WINDOW_H_
+#define WINDOW_H_
+
+class Window {
+public:
+	Window();
+	virtual ~Window();
+};
+
+#endif /* WINDOW_H_ */
