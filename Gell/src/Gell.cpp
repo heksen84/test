8294e3072ea493c785da21ca40193c98c57f4fed
Илья <heksen84@yandex.ru>
2017-09-24 23:46:58 +0600

    +

--- Gell/src/Gell.cpp
+++ Gell/src/Gell.cpp
@@ -175,7 +175,7 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 --------------------------------*/	
 int main(void){
 
-	sqlite3 *db;
+	/*sqlite3 *db;
 
 	char *zErrMsg;
 	const char *sql = "CREATE TABLE COMPANY('ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50), SALARY REAL')";
@@ -183,10 +183,10 @@ int main(void){
 
 	sqlite3_open(db_name, &db);
     sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-    sqlite3_close(db);
+    sqlite3_close(db);*/
 
 	//Msg::Info(L"Терезе алды құлпырды\nҚызыл, сары, көкала,\nШешек атты гүл түрлі,\nҮйір болды көп ара.\nҚызыға қарап тұрғанда\nШағып алды басымнан\nЕңбекшіні қуам ба\nБалын бізге тасыған!");
-	Msg::Info(L"Die Vögelein schweigen im Walde.");
+	//Msg::Info(L"Die Vögelein schweigen im Walde.");
 
 	InitLibs();
 	CreatePlane();
