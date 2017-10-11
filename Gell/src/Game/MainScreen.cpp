8e37fa169eb9d3da1aacffc8c49bb68002d760f5
Илья <heksen84@yandex.ru>
2017-10-11 20:06:36 +0600

    +

---
+++ Gell/src/Game/MainScreen.cpp
@@ -0,0 +1,23 @@
+/*
+ * MainScreen.cpp
+ *
+ *  Created on: 11 окт. 2017 г.
+ *      Author: Соня
+ */
+
+#include "MainScreen.h"
+#include "..//Common.h"
+
+MainScreen::MainScreen() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MainScreen::~MainScreen() {
+	// TODO Auto-generated destructor stub
+}
+
+void MainScreen::Render() {
+
+}
+
---
+++ Gell/src/Game/MainScreen.h
@@ -0,0 +1,18 @@
+/*
+ * MainScreen.h
+ *
+ *  Created on: 11 окт. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef MAINSCREEN_H_
+#define MAINSCREEN_H_
+
+class MainScreen {
+public:
+	MainScreen();
+	virtual ~MainScreen();
+	void Render();
+};
+
+#endif /* MAINSCREEN_H_ */
