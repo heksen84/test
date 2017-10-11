b227bf3fede5bc492f18855979681cef582982a5
Илья <heksen84@yandex.ru>
2017-10-11 23:34:51 +0600

    +

---
+++ Gell/src/ImageLoader.cpp
@@ -0,0 +1,12 @@
+/*
+ * ImageLoader.cpp
+ *
+ *  Created on: 11 окт. 2017 г.
+ *      Author: Соня
+ */
+
+#include "ImageLoader.h"
+
+void ImageLoader::LoadImage(const String &fileName)
+{
+}
---
+++ Gell/src/ImageLoader.h
@@ -0,0 +1,17 @@
+/*
+ * ImageLoader.h
+ *
+ *  Created on: 11 окт. 2017 г.
+ *      Author: Соня
+ */
+
+#ifndef IMAGELOADER_H_
+#define IMAGELOADER_H_
+#include "Common.h"
+
+class ImageLoader {
+public:
+	static void LoadImage(const String &fileName);
+};
+
+#endif /* IMAGELOADER_H_ */
