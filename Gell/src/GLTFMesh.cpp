982f4f5ec6fe10f5e299e25c403b0abfe4b0b5ea
Илья <heksen84@yandex.ru>
2018-04-29 13:05:37 +0600

    +

---
+++ Gell/src/GLTFMesh.cpp
@@ -0,0 +1,18 @@
+/*
+ * GLTFMesh.cpp
+ *
+ *  Created on: 29 апр. 2018 г.
+ *      Author: Соня
+ */
+
+#include "GLTFMesh.h"
+
+GLTFMesh::GLTFMesh() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GLTFMesh::~GLTFMesh() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Gell/src/GLTFMesh.h
@@ -0,0 +1,17 @@
+/*
+ * GLTFMesh.h
+ *
+ *  Created on: 29 апр. 2018 г.
+ *      Author: Соня
+ */
+
+#ifndef GLTFMESH_H_
+#define GLTFMESH_H_
+
+class GLTFMesh {
+public:
+	GLTFMesh();
+	virtual ~GLTFMesh();
+};
+
+#endif /* GLTFMESH_H_ */
